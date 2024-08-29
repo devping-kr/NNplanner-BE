51f8fda0085dc60511b6ad434785acf728b668ac
de123456sdf <cdaeun95@gmail.com>
2024-08-29 17:09:00 +0900

    fix: HttpStatus 로 수정

--- src/main/java/devping/nnplanner/global/exception/ErrorCode.java
+++ src/main/java/devping/nnplanner/global/exception/ErrorCode.java
@@ -2,18 +2,19 @@ package devping.nnplanner.global.exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
 
     //공통
-    NOT_FOUND(404, "리소스를 찾을 수 없습니다."),
-    BAD_REQUEST(400, "잘못된 요청입니다."),
-    UNAUTHORIZED(401, "인증되지 않은 접근입니다."),
-    FORBIDDEN(403, "접근이 금지되었습니다."),
-    INTERNAL_SERVER_ERROR(500, "서버 내부 오류입니다.");
+    NOT_FOUND(HttpStatus.NOT_FOUND, "리소스를 찾을 수 없습니다."),
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증되지 않은 접근입니다."),
+    FORBIDDEN(HttpStatus.FORBIDDEN, "접근이 금지되었습니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류입니다.");
 
-    private final int status;
+    private final HttpStatus status;
     private final String message;
 }
