a22b71f63e641a31756bbf453c22343c084062d6
de123456sdf <cdaeun95@gmail.com>
2024-09-03 19:44:24 +0900

    fix: 회원가입/로그인 구분 되도록 클래스명 변경

--- src/main/java/devping/nnplanner/domain/auth/dto/request/AuthSignRequestDTO.java
+++ src/main/java/devping/nnplanner/domain/auth/dto/request/AuthSignRequestDTO.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
-public class AuthRequestDTO {
+public class AuthSignRequestDTO {
 
     @NotBlank
     private String username;
