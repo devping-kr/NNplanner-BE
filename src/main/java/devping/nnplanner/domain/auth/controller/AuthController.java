4ab810052ad2d4606381408930baed268a914b70
de123456sdf <cdaeun95@gmail.com>
2024-09-04 13:10:07 +0900

    feature: 인증번호 발송/ 인증 구현

--- src/main/java/devping/nnplanner/domain/auth/controller/AuthController.java
+++ src/main/java/devping/nnplanner/domain/auth/controller/AuthController.java
@@ -1,10 +1,14 @@
 package devping.nnplanner.domain.auth.controller;
 
-import devping.nnplanner.domain.auth.dto.request.AuthRequestDTO;
+import devping.nnplanner.domain.auth.dto.request.AuthSignRequestDTO;
+import devping.nnplanner.domain.auth.dto.request.EmailCodeRequestDTO;
+import devping.nnplanner.domain.auth.dto.request.EmailRequestDTO;
 import devping.nnplanner.domain.auth.service.AuthService;
 import devping.nnplanner.domain.auth.service.EmailService;
 import devping.nnplanner.global.response.ApiResponse;
-import devping.nnplanner.global.response.ResponseUtil;
+import devping.nnplanner.global.response.GlobalResponse;
+import jakarta.mail.MessagingException;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,10 +26,33 @@ public class AuthController {
 
     @PostMapping("/signup")
     public ResponseEntity<ApiResponse<Void>> signUp(
-        @RequestBody AuthRequestDTO authRequestDTO) {
+        @RequestBody AuthSignRequestDTO authSignRequestDTO) {
 
-        authService.signUp(authRequestDTO);
+        authService.signUp(authSignRequestDTO);
 
-        return ResponseUtil.CREATED(ApiResponse.of("유저 회원가입 성공", null));
+        return GlobalResponse.CREATED("유저 회원가입 성공", null);
+    }
+
+    @PostMapping("/send")
+    public ResponseEntity<ApiResponse<Void>> sendEmail(
+        @RequestBody @Valid EmailRequestDTO emailRequestDTO)
+        throws MessagingException {
+
+        emailService.sendEmail(emailRequestDTO);
+
+        return GlobalResponse.OK("인증번호 발송 성공", null);
+    }
+
+    @PostMapping("/verify")
+    public ResponseEntity<ApiResponse<Void>> verifyEmail(
+        @RequestBody EmailCodeRequestDTO emailCodeRequestDTO) {
+
+        boolean isVerified = emailService.verifyEmail(emailCodeRequestDTO);
+
+        if (isVerified) {
+            return GlobalResponse.OK("이메일 인증 성공", null);
+        } else {
+            return GlobalResponse.BAD_REQUEST("이메일 인증 실패", null);
+        }
     }
 }
