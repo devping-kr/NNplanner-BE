a2f6678c30edc9d8076238c20b92ddfa40dadea3
de123456sdf <cdaeun95@gmail.com>
2024-09-22 15:47:36 +0900

    fix: 테이블명 설정

--- src/main/java/devping/nnplanner/domain/openapi/entity/ImportHospitalMenu.java
+++ src/main/java/devping/nnplanner/domain/openapi/entity/ImportHospitalMenu.java
@@ -4,11 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 
-/*
 @Table(name = "import_hospital_menus")
-*/
 @Getter
 @Entity
 public class ImportHospitalMenu {
