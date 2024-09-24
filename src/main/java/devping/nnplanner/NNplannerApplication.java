54f15cc20ccae88df176920591eeefec31a1abc3
de123456sdf <cdaeun95@gmail.com>
2024-09-24 14:45:29 +0900

    fix: 필요없는 어노테이션 삭제

--- src/main/java/devping/nnplanner/NNplannerApplication.java
+++ src/main/java/devping/nnplanner/NNplannerApplication.java
@@ -1,12 +1,10 @@
 package devping.nnplanner;
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-@EnableBatchProcessing
 @SpringBootApplication
 public class NNplannerApplication {
 
