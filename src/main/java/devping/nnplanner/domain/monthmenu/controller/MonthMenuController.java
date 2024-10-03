616b4a8483484ee299b1eb5d5c47e1cf0fc83567
de123456sdf <cdaeun95@gmail.com>
2024-10-03 13:33:49 +0900

    feature: 월별 식단 삭제 기능 구현

--- src/main/java/devping/nnplanner/domain/monthmenu/controller/MonthMenuController.java
+++ src/main/java/devping/nnplanner/domain/monthmenu/controller/MonthMenuController.java
@@ -19,6 +19,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -87,5 +88,15 @@ public class MonthMenuController {
 
         return GlobalResponse.OK("월별 식단 수정 성공", monthMenuResponseDTO);
     }
+
+    @DeleteMapping("/{monthMenuId}")
+    public ResponseEntity<ApiResponse<Void>> deleteMonthMenu(
+        @PathVariable("monthMenuId") UUID monthMenuId) {
+
+        monthMenuService.deleteMonthMenu(monthMenuId);
+
+        return GlobalResponse.OK("월별 식단 삭제 성공", null);
+    }
+
 
 }
