4d9f0b2d50c60e95b8d2e147e09246ed642ebcc8
de123456sdf <cdaeun95@gmail.com>
2024-10-09 00:38:08 +0900

    feature: 비밀번호 수정 api 구현

--- src/main/java/devping/nnplanner/domain/user/controller/UserController.java
+++ src/main/java/devping/nnplanner/domain/user/controller/UserController.java
@@ -8,6 +8,7 @@ import devping.nnplanner.global.response.GlobalResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,5 +30,15 @@ public class UserController {
 
         return GlobalResponse.OK("비밀번호 확인 성공", null);
 
+    }
+
+    @PatchMapping("edit-pw")
+    public ResponseEntity<ApiResponse<Void>> editPassword(
+        @AuthenticationPrincipal UserDetailsImpl userDetails,
+        @RequestBody UserRequestDTO userRequestDTO) {
+
+        userService.editPassword(userDetails, userRequestDTO);
+
+        return GlobalResponse.OK("비밀번호 수정 성공", null);
     }
 }
