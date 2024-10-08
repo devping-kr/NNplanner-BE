2831747debb0292be1e12c5c66da07b9fdee27f7
de123456sdf <cdaeun95@gmail.com>
2024-10-08 16:10:14 +0900

    feature: 작성한 식단 카테고리별 조회 api 구현

--- src/main/java/devping/nnplanner/domain/menucategory/controller/MenuCategoryController.java
+++ src/main/java/devping/nnplanner/domain/menucategory/controller/MenuCategoryController.java
@@ -1,11 +1,17 @@
 package devping.nnplanner.domain.menucategory.controller;
 
 import devping.nnplanner.domain.menucategory.service.MenuCategoryService;
+import devping.nnplanner.domain.monthmenu.dto.response.MonthMenuPageResponseDTO;
+import devping.nnplanner.global.jwt.user.UserDetailsImpl;
 import devping.nnplanner.global.response.ApiResponse;
 import devping.nnplanner.global.response.GlobalResponse;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,4 +32,23 @@ public class MenuCategoryController {
 
         return GlobalResponse.OK("소분류 목록 조회 성공", menuCategory);
     }
+
+    @GetMapping("/month-menus")
+    public ResponseEntity<ApiResponse<MonthMenuPageResponseDTO>> getAllMonthMenuByCategoryId(
+        @RequestParam("major-category") String majorCategory,
+        @RequestParam("minor-category") String minorCategory,
+        @AuthenticationPrincipal UserDetailsImpl userDetails,
+        @PageableDefault(page = 0, size = 8, sort = "createdAt",
+            direction = Sort.Direction.DESC) Pageable pageable) {
+
+        MonthMenuPageResponseDTO monthMenuPageResponseDTO =
+            menuCategoryService.getAllMonthMenuByCategoryId(
+                userDetails,
+                majorCategory,
+                minorCategory,
+                pageable);
+
+        return GlobalResponse.OK("작성한 식단 카테고리별 전체 조회 성공", monthMenuPageResponseDTO);
+    }
 }
+
