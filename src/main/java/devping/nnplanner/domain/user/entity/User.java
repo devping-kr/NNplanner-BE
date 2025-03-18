511b2604bd09601276d038382cc57a370b16b489
de123456sdf <cdaeun95@gmail.com>
2025-03-18 23:05:15 +0900

    fix: 구글 로그인시 패스워드가 null 이기 때문에 nullable 하게 변경

--- src/main/java/devping/nnplanner/domain/user/entity/User.java
+++ src/main/java/devping/nnplanner/domain/user/entity/User.java
@@ -30,7 +30,6 @@ public class User extends BaseTimeEntity {
     @Column(nullable = false)
     private String email;
 
-    @Column(nullable = false)
     private String password;
 
     @Column(nullable = false)
