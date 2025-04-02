207448f9dc80c7fc01cb6a9aa55d6622be0094e7
pinetree <eodnjs568@gmail.com>
2025-04-02 13:52:50 +0900

    chore : Survey 관련  DTO 수정

--- src/main/java/devping/nnplanner/domain/survey/entity/SurveyResponseDetail.java
+++ src/main/java/devping/nnplanner/domain/survey/entity/SurveyResponseDetail.java
@@ -1,16 +1,14 @@
 package devping.nnplanner.domain.survey.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 
 @Entity
 @Getter
 @Setter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class SurveyResponseDetail {
 
     @Id
@@ -25,22 +23,11 @@ public class SurveyResponseDetail {
     @JoinColumn(name = "question_id", nullable = false)
     private Question question;  // 질문과 연관
 
-    @Column(name = "answer_score")
-    private Integer answerScore;  // radio 타입 점수 저장
 
-    @Column(name = "answer_text", length = 1000)
-    private String answerText;  // text 타입의 단일 텍스트 응답 저장
 
-    @ElementCollection
-    @CollectionTable(name = "survey_response_answer_list", joinColumns = @JoinColumn(name = "survey_response_detail_id"))
-    @Column(name = "answer_item")
-    private List<String> answerList;  // text 타입의 리스트 응답 저장 (ex: "가장 좋아하는 상위 3개 식단")
-
-    public SurveyResponseDetail(SurveyResponse surveyResponse, Question question, Integer answerScore, String answerText, List<String> answerList) {
+    @Builder
+    private SurveyResponseDetail(SurveyResponse surveyResponse, Question question) {
         this.surveyResponse = surveyResponse;
         this.question = question;
-        this.answerScore = answerScore;
-        this.answerText = answerText;
-        this.answerList = answerList;
     }
 }
